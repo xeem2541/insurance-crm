8bd822ea91d76453a167b842ecf76b8ad01aeb78
xeem2541 <xeem2541@gmail.com>
2026-06-07 12:30:43 +0700

    Add files via upload

--- dc/frontend/vite.config.js
+++ dc/frontend/vite.config.js
@@ -4,6 +4,25 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern-compiler'
+      }
+    }
+  },
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['react-bootstrap', 'bootstrap'],
+          utils: ['jspdf', 'html2canvas', 'moment', 'react-big-calendar']
+        }
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 5173
