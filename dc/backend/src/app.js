0ac36489d8eda578a707df779381dd500f5de358
xeem2541 <xeem2541@gmail.com>
2026-06-10 15:58:31 +0700

    Add files via upload

--- dc/backend/src/app.js
+++ dc/backend/src/app.js
@@ -33,6 +33,7 @@ const pool = mysql.createPool({
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
+  dateStrings: true,
   ssl: process.env.DB_SSL === 'true' ? { rejectUnauthorized: true } : undefined
 });
 
