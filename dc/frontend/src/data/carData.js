9f33e04ad2f2d007ea15bc7dfe2d8da3fac7efa2
xeem2541 <xeem2541@gmail.com>
2026-06-11 03:09:12 +0700

    Add files via upload

--- dc/frontend/src/data/carData.js
+++ dc/frontend/src/data/carData.js
@@ -1,17 +1,37 @@
 export const carBrands = [
-  "Toyota", "Honda", "Isuzu", "Mitsubishi", "Nissan", "Mazda", "Ford", "MG", "Suzuki", "BMW", "Mercedes-Benz", "Hyundai", "Kia", "Subaru", "Volvo", "Porsche"
+  "Toyota", "Honda", "Isuzu", "Mitsubishi", "Nissan", "Mazda", "Ford", "MG", "Suzuki", 
+  "BMW", "Mercedes-Benz", "Hyundai", "Kia", "Subaru", "Volvo", "Porsche",
+  "BYD", "GWM (Haval/ORA)", "Neta", "Tesla", "Lexus", "Audi", "Chevrolet", 
+  "Deepal", "AION", "Peugeot", "Wuling", "Other (อื่นๆ)"
 ];
 
 export const carModels = {
-  "Toyota": ["Yaris", "Yaris Cross", "Vios", "Corolla Altis", "Camry", "Hilux Revo", "Hilux Vigo", "Fortuner", "Corolla Cross", "C-HR", "Veloz", "Alphard", "Majesty"],
-  "Honda": ["City", "Civic", "Accord", "HR-V", "CR-V", "BR-V", "WR-V", "Jazz", "Brio", "Mobilio"],
-  "Isuzu": ["D-Max", "MU-X", "MU-7"],
-  "Mitsubishi": ["Triton", "Pajero Sport", "Xpander", "Xpander Cross", "Mirage", "Attrage"],
-  "Nissan": ["Almera", "March", "Kicks", "Navara", "Terra", "Sylphy", "Teana", "Note"],
-  "Mazda": ["Mazda2", "Mazda3", "CX-3", "CX-30", "CX-5", "CX-8", "BT-50"],
-  "Ford": ["Ranger", "Everest", "Raptor", "Mustang", "Fiesta", "Focus"],
-  "MG": ["MG3", "MG5", "MG ZS", "MG HS", "MG EP", "MG4", "MG VS", "MG Maxus 9"],
-  "Suzuki": ["Swift", "Celerio", "Ciaz", "Ertiga", "XL7", "Carry"],
-  "BMW": ["X1", "X3", "X5", "Series 2", "Series 3", "Series 4", "Series 5", "Series 7"],
-  "Mercedes-Benz": ["A-Class", "C-Class", "E-Class", "S-Class", "GLA", "GLC", "GLE", "GLS"]
+  "Toyota": ["Yaris", "Yaris Ativ", "Yaris Cross", "Vios", "Corolla Altis", "Corolla Cross", "Camry", "Hilux Revo", "Hilux Vigo", "Fortuner", "C-HR", "Veloz", "Alphard", "Majesty", "Innova", "Innova Zenix", "Sienta", "Avanza", "bZ4X", "Prius", "Commuter", "Hiace"],
+  "Honda": ["City", "Civic", "Accord", "HR-V", "CR-V", "BR-V", "WR-V", "Jazz", "Brio", "Brio Amaze", "Mobilio", "Freed", "Odyssey", "Stepwgn"],
+  "Isuzu": ["D-Max", "MU-X", "MU-7", "V-Cross", "Spark", "Spacecab", "Cab4"],
+  "Mitsubishi": ["Triton", "Pajero Sport", "Xpander", "Xpander Cross", "Mirage", "Attrage", "Lancer", "Lancer EX", "Space Wagon"],
+  "Nissan": ["Almera", "March", "Kicks", "Navara", "Terra", "Sylphy", "Teana", "Note", "X-Trail", "Juke", "Pulsar", "Tiida", "Leaf"],
+  "Mazda": ["Mazda2", "Mazda3", "CX-3", "CX-30", "CX-5", "CX-8", "BT-50", "BT-50 Pro", "MX-5"],
+  "Ford": ["Ranger", "Ranger Raptor", "Everest", "Mustang", "Fiesta", "Focus", "EcoSport", "Escape", "Territory"],
+  "MG": ["MG3", "MG5", "MG ZS", "MG ZS EV", "MG HS", "MG HS PHEV", "MG EP", "MG4", "MG VS", "MG Maxus 9", "MG Extender", "MG GS"],
+  "Suzuki": ["Swift", "Celerio", "Ciaz", "Ertiga", "XL7", "Carry", "Jimny", "SX4"],
+  "BMW": ["X1", "X2", "X3", "X4", "X5", "X6", "X7", "Series 1", "Series 2", "Series 3", "Series 4", "Series 5", "Series 6", "Series 7", "Series 8", "iX", "iX3", "i4", "i5", "i7", "Z4"],
+  "Mercedes-Benz": ["A-Class", "B-Class", "C-Class", "E-Class", "S-Class", "GLA", "GLB", "GLC", "GLE", "GLS", "G-Class", "CLA", "CLS", "EQS", "EQE", "EQA", "EQB"],
+  "Hyundai": ["Staria", "H-1", "Creta", "Tucson", "Santa Fe", "Ioniq 5", "Ioniq 6", "Elantra", "Sonata"],
+  "Kia": ["Carnival", "Sorento", "Sportage", "EV6", "EV9", "Cerato"],
+  "Subaru": ["XV", "Forester", "Outback", "BRZ", "WRX"],
+  "Volvo": ["XC40", "XC60", "XC90", "S60", "S90", "V60", "V90", "EX30", "C40"],
+  "Porsche": ["Cayenne", "Macan", "Panamera", "Taycan", "911", "718 Boxster", "718 Cayman"],
+  "BYD": ["Atto 3", "Dolphin", "Seal", "Sealion 6", "Tang"],
+  "GWM (Haval/ORA)": ["Haval H6", "Haval Jolion", "ORA Good Cat", "ORA 07", "Tank 300", "Tank 500"],
+  "Neta": ["Neta V", "Neta V-II", "Neta X"],
+  "Tesla": ["Model 3", "Model Y", "Model S", "Model X"],
+  "Lexus": ["IS", "ES", "LS", "NX", "RX", "UX", "LM", "RZ"],
+  "Audi": ["A3", "A4", "A5", "A6", "A7", "A8", "Q2", "Q3", "Q5", "Q7", "Q8", "e-tron", "TT"],
+  "Chevrolet": ["Colorado", "Trailblazer", "Captiva", "Cruze", "Sonic", "Optra", "Aveo"],
+  "Deepal": ["L07", "S07", "Lumin"],
+  "AION": ["AION Y Plus", "AION ES"],
+  "Peugeot": ["2008", "3008", "5008"],
+  "Wuling": ["Air EV", "Binguo EV", "Mini EV"],
+  "Other (อื่นๆ)": []
 };
