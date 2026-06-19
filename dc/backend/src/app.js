482a21c26b35f5e27a7d23dcd8f9c80012258490
xeem2541 <xeem2541@gmail.com>
2026-06-19 22:34:48 +0700

    Add files via upload

--- dc/backend/src/app.js
+++ dc/backend/src/app.js
@@ -18,6 +18,8 @@ try {
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.set('trust proxy', 1);
+
 app.use(helmet({
   crossOriginResourcePolicy: false, // allow cross-origin images/resources if needed
 }));
