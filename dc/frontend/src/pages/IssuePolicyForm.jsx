9f8031ea295f55845d1f4b7b477d59827b694a77
xeem2541 <xeem2541@gmail.com>
2026-06-19 20:17:56 +0700

    Add files via upload

--- dc/frontend/src/pages/IssuePolicyForm.jsx
+++ dc/frontend/src/pages/IssuePolicyForm.jsx
@@ -299,7 +299,7 @@ const IssuePolicyForm = () => {
     try {
       // Compress images in parallel before sending to backend to speed up upload & processing
       const compressedFiles = await Promise.all(
-        rawFiles.map(file => compressImage(file))
+        rawFiles.map(file => compressImage(file, 1200, 1200, 0.6))
       );
 
       const formData = new FormData();
